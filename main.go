7c6bfe1876e68466bd1851e66f949baa20e50abf
Izac Peterson <izac@Izac-PC.lan>
2024-06-06 10:49:50 -0600

    cd

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -31,6 +32,13 @@ func execInput(input string) error {
 	input = strings.TrimSuffix(input, "\n")
 	args := strings.Split(input, " ")
 	cmd := exec.Command(args[0], args[1:]...)
+
+	if args[0] == "cd" {
+		if len(args) < 2 {
+			return errors.New("path required")
+		}
+		return os.Chdir(args[1])
+	}
 
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
